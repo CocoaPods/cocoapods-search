f1b55561c051b4396a88cbd4ea777baefe696ebc
Samuel Giddins <segiddins@segiddins.me>
2016-04-29 21:37:41 -0500

    Release 1.0.0.rc.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Cocoapods::Search Changelog
 
-## Master
+## 1.0.0.rc.1 (2016-04-30)
 
 ##### Enhancements
 
--- lib/cocoapods-search/gem_version.rb
+++ lib/cocoapods-search/gem_version.rb
@@ -1,3 +1,3 @@
 module CocoapodsSearch
-  VERSION = '1.0.0.beta.2'.freeze
+  VERSION = '1.0.0.rc.1'.freeze
 end
